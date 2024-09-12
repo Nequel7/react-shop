b594ca90feed2557c41474c11a36e1704cf072a4
Ivan Zubrik <zubria2003@gmail.com>
2024-09-13 01:51:48 +0300

    add repo url

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
         <div className="nav-wrapper">
             <a href="#" className="brand-logo">Nequel Shop</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="https://github.com/Nequel7/react-shop" target="_blank">Repo</a></li>
+                <li><a href="https://github.com/Nequel7/react-shop" target="_blank" rel="noreferrer">Repo</a></li>
 
             </ul>
         </div>
